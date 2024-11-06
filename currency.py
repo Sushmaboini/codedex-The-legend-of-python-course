285f7385a7b9629a2401ab77bcbaf62e71760e7e
Sushma Boini <sushmaboini899@gmail.com>
2024-11-06 18:37:09 +0530

    Create currency.py

---
+++ currency.py
@@ -0,0 +1,17 @@
+# Currency exchange 
+
+pesos = int(input("what do you have left in pesos:"))
+
+soles = int(input("what do you have left in soles:"))
+
+reais = int(input("what do you have left in reais"))
+
+pesos1 = pesos*0.050
+
+soles1 = soles*0.26
+
+reais1 = reais*0.17
+
+total = pesos1 + soles1 + reais1
+
+print(total)
