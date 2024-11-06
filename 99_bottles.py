5488ff9b29e47d98bae78c1a5ff1504e16f18851
Sushma Boini <sushmaboini899@gmail.com>
2024-11-06 18:29:19 +0530

    Create 99_bottles.py

---
+++ 99_bottles.py
@@ -0,0 +1,12 @@
+# String concatenation
+
+for i in range(99,0,-1):
+  print({i},'bottles of beer on the wall.',{i},'bottles of beer.')
+if i != 1:
+  print({i},'If one of the bottles just happen to fall,',{i-1},'bottles of beer on the wall.')
+else:
+  print('No more bottles of beer on the wall,')
+  print('no more bottles of beer.')
+  print('We have taken them downand passed them around;')
+  print('now we are drunk and passed out!')
+
